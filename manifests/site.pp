5e658f3b3ddc73c1e77f44d32403d343a2cdc5be
Bécaye Badiane <becaye00@gmail.com>
2021-04-21 06:09:19 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,10 +4,10 @@ node 'master.puppet.com' {
   include role::master_server
 }
 
-node web /^web/ {
+node /^web/ {
   include role::app_server
 }
 
-node web /^db/ {
+node /^db/ {
   include role::db_server
 }
