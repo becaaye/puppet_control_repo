ff656b0889276aa877cbee52d1eccb0a06e520ea
Bécaye Badiane <becaye00@gmail.com>
2021-04-21 05:44:47 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default{
-  file { '/root/README':
-    ensure => file,
-    content => 'this is a readme',
-    owner => 'root',
-  }
+}
+node 'master.puppet.com' {
+  include role::master_server
 }
